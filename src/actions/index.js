43da1082e3925d18571563b565625931a3abfb2f
mbrunetto <matteo.brunetto@lastminute.com>
2018-09-14 20:15:09 +0200

    Refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,17 @@
 let _id = 1;
+
 export function uniqueId() {
     return _id++;
 }
 
-export function createTask({ title, description }) {
+export function createTask({title, description}) {
     return {
         type: 'CREATE_TASK',
         payload: {
             id: uniqueId(),
-        title,
-        description,
-        status: 'Unstarted',
-},
-};
+            title,
+            description,
+            status: 'Unstarted'
+        },
+    };
 }
