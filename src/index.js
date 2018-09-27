4aae123d14dacb7646b649684f8053b60c5074c3
mbrunetto <matteo.brunetto@lastminute.com>
2018-09-27 23:20:20 +0200

    create an analytics middleware

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import tasksReducer from './reducers';
 import logger from './middleware/logger';
+import analytics from './middleware/analytics';
 
 const rootReducer = (state = {}, action) => {
     return {
@@ -17,7 +18,7 @@ const rootReducer = (state = {}, action) => {
     };
 };
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger, analytics)));
 
 ReactDOM.render(
     <Provider store={store}>
