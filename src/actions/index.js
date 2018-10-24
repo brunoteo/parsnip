e43351b0f3118af94ddfaefe2365b146ca828049
mbrunetto <matteo.brunetto@lastminute.com>
2018-10-24 23:30:51 +0200

    remove comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -54,48 +54,6 @@ function progressTimerStop(taskId) {
     return { type: 'TIMER_STOPPED', payload: { taskId } };
 }
 
-// function fetchTasksStarted() {
-//     return {
-//         type: 'FETCH_TASKS_STARTED',
-//     };
-// }
-
-// export function fetchTasks() {
-//     return dispatch => {
-//         dispatch(fetchTasksStarted());
-//
-//         api.fetchTasks()
-//             .then(resp => {
-//                 setTimeout(() => {
-//                     dispatch(fetchTasksSucceeded(resp.data));
-//                 }, 2000);
-//                 // throw new Error('Oh noes! Unable to fetch tasks!');
-//             })
-//             .catch(err => {
-//                 dispatch(fetchTasksFailed(err.message));
-//             })
-//     };
-// }
-
-// export function fetchTasksSucceeded(tasks) {
-//     return {
-//         type: 'FETCH_TASKS_SUCCEEDED',
-//         payload: {
-//             tasks
-//         }
-//     }
-// }
-
-// function fetchTasksFailed(error) {
-//     return {
-//         type: 'FETCH_TASKS_FAILED',
-//         payload: {
-//             error,
-//         },
-//     };
-// }
-
-//Only for sagas
 export function fetchTasks() {
     return { type: 'FETCH_TASKS_STARTED' };
 }
