79b82a9a43d58a77e0d23153eef58a4270a6bce5
mbrunetto <matteo.brunetto@lastminute.com>
2018-10-24 22:47:01 +0200

    introduce sagas

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import App from './App';
 import './index.css';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import tasksReducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = (state = {}, action) => {
     return {
@@ -16,7 +20,9 @@ const rootReducer = (state = {}, action) => {
     };
 };
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
